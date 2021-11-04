38de2aaefa07b57997544c869f68771b27340390
PsPLoG <gkgkgkrj@gmail.com>
2021-11-04 18:43:35 +0900

    test

--- app/src/main/java/com/drunkenboys/calendarun/MainActivity.kt
+++ app/src/main/java/com/drunkenboys/calendarun/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.drunkenboys.calendarun
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.drunkenboys.calendarun.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.toolbar2.setOnClickListener {
+            binding.calendarMonth.isVisible = binding.calendarMonth.isVisible.not()
+            binding.calendarYear.isVisible = binding.calendarYear.isVisible.not()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_delete_schedule -> {
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
